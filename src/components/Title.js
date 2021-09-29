f956fa9b731ca64af2dcf0e64ad8ea7c7a20cc2b
Onea <onea8906@gmail.com>
2021-09-29 21:33:03 +0900

    feat: apply react quill editor

---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const Title = () => {
+  return (
+    <div css={title}>
+      EASYME.md
+    </div>
+  );
+}
+
+const title = css`
+  margin-top: 3rem;
+  text-align: center;
+  color: goldenrod;
+  font-size: 4rem;
+`;
+
+export default Title;
