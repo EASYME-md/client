1d59b4f97696766671dc6d2a58e3b0489b8d4d0c
Onea <onea8906@gmail.com>
2021-10-04 04:31:10 +0900

    refactor: change isLoading position

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import './App.css';
 
-import Title from './components/Title';
-import Editor from './components/Editor';
-import Preview from './components/Preview';
-import SharingButton from './components/SharingButton';
+import Home from './components/Home';
 
 const App = () => {
   const linkId = nanoid(10);
@@ -15,16 +12,10 @@ const App = () => {
     <>
       <Switch>
         <Route path={`/${linkId}`}>
-          <SharingButton />
-          <Title />
-          <Editor />
-          <Preview />
+          <Home />
         </Route>
         <Route path='/:linkId'>
-          <SharingButton />
-          <Title />
-          <Editor />
-          <Preview />
+          <Home />
         </Route>
       </Switch>
       <Route path='/' exact>
