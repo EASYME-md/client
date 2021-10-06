4a87c27cf56a3e36729cc9262530b12c7af2df7f
Onea <onea8906@gmail.com>
2021-10-07 00:51:12 +0900

    refactor: change list toolbar from Quill to textarea

--- src/components/CustomTools/CustomBlockQuote.js
+++ src/components/CustomTools/CustomBlockQuote.js
@@ -3,14 +3,14 @@ import { FaQuoteLeft } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { addText } from '../../features/slice';
-import addTypeCurrentColumn from '../../utils/addTypeCurrentColumn';
+import addTypeCurrentRow from '../../utils/addTypeCurrentRow';
 
 const CustomBlockQuote = () => {
   const dispatch = useDispatch();
   const { textArea } = useSelector((state) => state.contents);
 
   const handleButton = () => {
-    const resultValue = addTypeCurrentColumn(textArea, '>');
+    const resultValue = addTypeCurrentRow(textArea, '>');
 
     dispatch(addText(resultValue));
   };
