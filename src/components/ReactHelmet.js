01407db69ea070129f019595ef2eea44243c0d9d
Onea <onea8906@gmail.com>
2021-10-13 19:36:47 +0900

    chore: modify something for build error

--- src/components/ReactHelmet.js
+++ src/components/ReactHelmet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import favicon from '../assets/easyme.ico';
 import image from '../assets/thumbnail.png';
@@ -9,24 +9,26 @@ const ReactHelmet = () => {
   const description = 'README.md를 쉽게 작성하는 방법, EASYME.md!';
 
   return (
-    <Helmet>
-      <title>{title}</title>
-      <link rel='icon' href={favicon} />
-      <meta charSet='utf-8' />
-      <meta name='description' content={description} />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <link rel='icon' href={favicon} />
+        <meta charSet='utf-8' />
+        <meta name='description' content={description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
 
-      <meta property='og:type' content='website' />
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      <meta property='og:image' content={image} />
-      <meta property='og:url' content='url' />
-      <meta property='og:site_name' content={title} />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:image' content={image} />
+        <meta property='og:url' content='url' />
+        <meta property='og:site_name' content={title} />
 
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={image} />
-    </Helmet>
+        <meta name='twitter:title' content={title} />
+        <meta name='twitter:description' content={description} />
+        <meta name='twitter:image' content={image} />
+      </Helmet>
+    </HelmetProvider>
   )
 };
 
